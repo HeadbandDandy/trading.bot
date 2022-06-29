0de43ec517d7ad2a3b4b56a7f6554c06af921c49
Darryl Johnson <dandycodes@gmail.com>
2022-06-29 19:27:10 -0400

    Alpaca API Modules added 6/29

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const Alpaca = require('@alpacahq/alpaca-trade-api')
+
+const alpaca = new Alpaca({
+    keyId: PKVTPATP4VGGB3XCPWWS,
+    secretKey: o854Bq8cM8fJPC31hQxs9WUxyH1MFftMbwAz3pc3,
+    paper: true
+})
