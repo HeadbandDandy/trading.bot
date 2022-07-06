63a2c38c4cbf819fa99a8072955ae0f73f1ff927
Darryl Johnson <dandycodes@gmail.com>
2022-07-06 00:29:58 -0400

    need to fix "require" bug

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
 const Alpaca = require('@alpacahq/alpaca-trade-api')
+const API_KEY = "PKVTPATP4VGGB3XCPWWS";
+const API_SECRET = "o854Bq8cM8fJPC31hQxs9WUxyH1MFftMbwAz3pc3";
 
-const alpaca = new Alpaca({
-    keyId: PKVTPATP4VGGB3XCPWWS,
-    secretKey: o854Bq8cM8fJPC31hQxs9WUxyH1MFftMbwAz3pc3,
-    paper: true
+class DataStream {
+    constructor ({apiKey, secretKey, feed}) {
+        this.alpaca = new Alpaca ({
+            keyId: apiKey,
+            secretKey,
+            feed,
+        })
+    }
+}
+
+const socket = this.alpac.data_stream_v2;
+
+socket.onConnect(function (){
+    console.log("Connected");
+    socket.subscribeForQuotes(['AAPL'])
 })
+
